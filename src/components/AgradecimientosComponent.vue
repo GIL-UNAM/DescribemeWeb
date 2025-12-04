4aaac92e960e452c55f8af7e2e2dce7262099ae5
Cesar Cabrera <cesarcabreradev@protonmail.com>
2025-12-03 18:55:14 -0600

    Actualicé los agradecimientos

--- src/components/AgradecimientosComponent.vue
+++ src/components/AgradecimientosComponent.vue
@@ -2,6 +2,36 @@
     <div id="descripcion" class="d-flex flex-column ga-4">
         <p class="text-subtitle-1 text-sm-h6 font-weight-bold"> Agradecimientos</p>
         <p class="text-body text-sm-subtitle-1">
+            Coordinación del proyecto: 
+        </p>
+        <ul class="listas">
+            <li class="font-weight-bold text-sm-subtitle-1">
+                Gerardo Sierra Martínez
+            </li>
+        </ul>
+        <p class="text-body text-sm-subtitle-1">
+            Diseño del algorítmo: 
+        </p>
+        <ul class="listas">
+            <li class="font-weight-bold text-sm-subtitle-1">
+                Jorge Reyes Magaña
+            </li>
+            <li class="font-weight-bold text-sm-subtitle-1">
+                Helena Gómez Adorno
+            </li>
+            <li class="font-weight-bold text-sm-subtitle-1">
+                Gemma Bel-Enguix
+            </li>
+        </ul>
+        <p class="text-body text-sm-subtitle-1">
+            Coordinación informática: 
+        </p>
+        <ul class="listas">
+            <li class="font-weight-bold text-sm-subtitle-1">
+                Gabriel Castillo Hernández
+            </li>
+        </ul>
+        <p class="text-body text-sm-subtitle-1">
             Agradecemos el patrocinio del CONACyT a través del proyecto: 
         </p>
         <ul class="listas">
@@ -9,7 +39,7 @@
                 CONACYT CF-2023-G-64 (Grafos conceptuales para la contrucción de diccionarios inversos en áreas de especialidad)
             </li>
         </ul>
-        <p class="text-body text-sm-subtitle-1"> Agradecemos también la programación del algorithmo en a:</p>
+        <p class="text-body text-sm-subtitle-1"> Agradecemos también la programación del algorítmo a:</p>
         <ul class="listas">
             <li class="font-weight-bold text-sm-subtitle-1">
                 Regina Arahí Guerra Hernández
